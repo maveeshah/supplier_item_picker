520bf00b039da61acdc0d1776b550ea3e8f83ca6
Ameer Muavia Shah <mavee.shah@hotmail.com>
2024-05-25 11:02:24 +0500

    feat: adding custom report for billed items not delivered

---
+++ supplier_item_picker/supplier_item_picker/report/billed_items_to_be_delivered/billed_items_to_be_delivered.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2024, Ameer Muavia Shah and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["Billed Items To Be Delivered"] = {
+
+	filters: [
+
+	],
+};
---
+++ supplier_item_picker/supplier_item_picker/report/billed_items_to_be_delivered/billed_items_to_be_delivered.py
@@ -0,0 +1,8 @@
+import frappe
+from frappe.utils import flt
+from frappe import _
+
+
+def execute(filters=None):
+    columns, data = [], []
+    return columns, data
