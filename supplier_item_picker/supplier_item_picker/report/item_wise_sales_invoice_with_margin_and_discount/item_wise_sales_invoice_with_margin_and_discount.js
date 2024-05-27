e803336fbeccbfa41e9e8035a6e9bdeea449667a
Ameer Muavia Shah <mavee.shah@hotmail.com>
2024-05-27 21:13:54 +0500

    feat: added a report Item-wise Sales Invoice with Margin and Discount

---
+++ supplier_item_picker/supplier_item_picker/report/item_wise_sales_invoice_with_margin_and_discount/item_wise_sales_invoice_with_margin_and_discount.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Ameer Muavia Shah and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["Item-wise Sales Invoice with Margin and Discount"] = {
+	"filters": [
+
+	]
+};
