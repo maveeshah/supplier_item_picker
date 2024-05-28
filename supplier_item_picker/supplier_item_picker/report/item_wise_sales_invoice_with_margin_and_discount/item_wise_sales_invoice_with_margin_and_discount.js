e845365b1980c27a6036caf31820ec8de40f3d8a
Ameer Muavia Shah <mavee.shah@hotmail.com>
2024-05-28 09:51:55 +0500

    feat: added filters in item wise report

--- supplier_item_picker/supplier_item_picker/report/item_wise_sales_invoice_with_margin_and_discount/item_wise_sales_invoice_with_margin_and_discount.js
+++ supplier_item_picker/supplier_item_picker/report/item_wise_sales_invoice_with_margin_and_discount/item_wise_sales_invoice_with_margin_and_discount.js
@@ -1,8 +1,58 @@
 // Copyright (c) 2024, Ameer Muavia Shah and contributors
 // For license information, please see license.txt
 
-frappe.query_reports["Item-wise Sales Invoice with Margin and Discount"] = {
-	"filters": [
+// Copyright (c) 2024, Ameer Muavia Shah and contributors
+// For license information, please see license.txt
 
-	]
+frappe.query_reports["Item-wise Sales Invoice with Margin and Discount"] = {
+	filters: [
+		{
+			label: __("Customer"),
+			fieldname: "customer",
+			fieldtype: "Link",
+			options: "Customer",
+		},
+		{
+			label: __("Customer Group"),
+			fieldname: "customer_group",
+			fieldtype: "Link",
+			options: "Customer Group",
+		},
+		{
+			label: __("Mode of Payment"),
+			fieldname: "mode_of_payment",
+			fieldtype: "Link",
+			options: "Mode of Payment",
+		},
+		{
+			label: __("Owner"),
+			fieldname: "owner",
+			fieldtype: "Link",
+			options: "User",
+		},
+		{
+			label: __("Cost Center"),
+			fieldname: "cost_center",
+			fieldtype: "Link",
+			options: "Cost Center",
+		},
+		{
+			label: __("Warehouse"),
+			fieldname: "warehouse",
+			fieldtype: "Link",
+			options: "Warehouse",
+		},
+		{
+			label: __("Brand"),
+			fieldname: "brand",
+			fieldtype: "Link",
+			options: "Brand",
+		},
+		{
+			label: __("Item Group"),
+			fieldname: "item_group",
+			fieldtype: "Link",
+			options: "Item Group",
+		},
+	],
 };
