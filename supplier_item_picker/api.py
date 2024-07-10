deaadfe8c3e3157f2990ab32d5a44759e49c6784
Ameer Muavia Shah <mavee.shah@hotmail.com>
2024-07-10 11:58:02 +0500

    feat: barcode changes

--- supplier_item_picker/api.py
+++ supplier_item_picker/api.py
@@ -1,5 +1,12 @@
 import frappe
 import json
+import os
+from barcode import EAN13
+from barcode.writer import ImageWriter
+import cv2
+from random import randrange
+from functools import reduce
+import base64
 
 
 @frappe.whitelist()
@@ -60,3 +67,104 @@ def get_items_details(items):
     )
 
     return items_details or []  # Return an empty list if no details are found
+
+
+def listToString(s):
+
+    # initialize an empty string
+    str1 = ""
+
+    # traverse in the string
+    for ele in s:
+        str1 += str(ele)
+
+    # return string
+    return str1
+
+
+def generate_12_random_numbers():
+    numbers = []
+    for x in range(12):
+        numbers.append(randrange(10))
+    return numbers
+
+
+def calculate_checksum(ean):
+    """
+    Calculates the checksum for an EAN13
+    @param list ean: List of 12 numbers for first part of EAN13
+    :returns: The checksum for `ean`.
+    :rtype: Integer
+    """
+    assert len(ean) == 12, "EAN must be a list of 12 numbers"
+    sum_ = lambda x, y: int(x) + int(y)
+    evensum = reduce(sum_, ean[::2])
+    oddsum = reduce(sum_, ean[1::2])
+    return (10 - ((evensum + oddsum * 3) % 10)) % 10
+
+
+@frappe.whitelist()
+def create_code(
+    number="", item_code="new_code1", item_name="new_code1", file_name="Itemcode"
+):
+    try:
+        if number == "":
+            number = generate_12_random_numbers()
+            number.append(calculate_checksum(number))
+            number = listToString(number)
+
+        if len(number) != 13 or not number.isdigit():
+            return {"status": "error", "message": "Invalid EAN-13 number"}
+
+        file_name = file_name + "_" + number
+        my_code = EAN13(number, writer=ImageWriter())
+
+        output_file_folder = frappe.utils.get_site_path("public", "files")
+
+        if not os.path.exists(output_file_folder):
+            os.makedirs(output_file_folder)
+
+        output_file_path = os.path.join(output_file_folder, f"{file_name}")
+
+        my_code.save(output_file_path)
+
+        image = cv2.imread(output_file_path + ".png")
+
+        h, w, c = image.shape
+        text = item_code + "  " + item_name
+        font = cv2.FONT_HERSHEY_SIMPLEX
+
+        fontScale = 0.5
+
+        color = (0, 0, 0)
+
+        thickness = 1
+
+        textsize = cv2.getTextSize(text, font, fontScale, thickness)[0]
+
+        offset = int(h - 10)
+
+        textX = (image.shape[1] - textsize[0]) // 2
+        org = (textX, offset)
+
+        image = cv2.putText(
+            image, text, org, font, fontScale, color, thickness, cv2.LINE_AA, False
+        )
+
+        cv2.imwrite(output_file_path + ".png", image)
+
+        with open(output_file_path + ".png", "rb") as imagefile:
+            convert = base64.b64encode(imagefile.read())
+
+        return {
+            "status": "success",
+            "message": {
+                # "base": "data:image/png;base64," + convert.decode("utf-8"),
+                "base": convert.decode("utf-8"),
+                "image": "/files/" + file_name + ".png",
+                "barcode": number,
+            },
+        }
+
+    except Exception as e:
+        return {"status": "error", "message": str(e)}
--- supplier_item_picker/public/js/item.js
+++ supplier_item_picker/public/js/item.js
@@ -1,91 +1,22 @@
-frappe.ui.form.on('Item', {
-    refresh: function (frm) {
-        // frm.add_custom_button(__('Generate Barcode from Item Name'), function () {
-        //     var cur_doc = frm.doc;
-        //     if (cur_doc.name) {
-        //         $(frm.fields_dict['custom_barcode_image'].wrapper).html('<svg id="ean13"></svg>');
-        //         $.getScript("https://cdn.jsdelivr.net/npm/jsbarcode@3.11.5/dist/JsBarcode.all.min.js", function (data, textStatus, jqxhr) {
-        //             JsBarcode("#ean13", cur_doc.name, {
-        //                 background: "#FFFFFF"
-        //             });
-        //             var svg = $('#ean13').parent().html();
-        //             frappe.model.set_value(cur_doc.doctype, cur_doc.name, "custom_barcode_svg", svg);
-        //             frappe.model.set_value(cur_doc.doctype, cur_doc.name, "custom_barcode_generated", 1);
-        //             cur_frm.save();
-        //         });
-        //     } else {
-        //         frappe.msgprint(__("Please enter Barcode to to be printed"));
-        //     }
-
-        //     //     var item_name = frm.doc.item_name;
-
-        //     //     if (item_name) {
-        //     //         var barcodeFieldWrapper = $(frm.fields_dict.barcodes.wrapper);
-        //     //         var barcodeTable = barcodeFieldWrapper.find('.form-inner-toolbar').next('.form-grid').find('.grid-body');
-
-        //     //         // Create a new row in the barcodes table
-        //     //         var newRow = $('<div class="grid-row" data-idx="0"></div>').appendTo(barcodeTable);
-        //     //         var cellWrapper = $('<div class="grid-static-col col col-xs-6 col-sm-6 col-md-6 col-lg-6"></div>').appendTo(newRow);
-
-        //     //         // Add the custom_barcode_image field to the new row
-        //     //         var imageField = $('<img class="frappe-control">').appendTo(cellWrapper);
-
-        //     //         // Load JsBarcode script
-        //     //         $.getScript("https://cdn.jsdelivr.net/npm/jsbarcode@3.11.5/dist/JsBarcode.all.min.js", function (data, textStatus, jqxhr) {
-        //     //             try {
-        //     //                 JsBarcode(imageField[0], item_name, {
-        //     //                     format: 'CODE128', // You can use other formats as needed
-        //     //                     lineColor: "#000",
-        //     //                     width: 2,
-        //     //                     height: 100,
-        //     //                     displayValue: true,
-        //     //                     background: "#FFFFFF"
-        //     //                 });
-        //     //                 console.log('hi');
-        //     //                 console.log('Generated Barcode Image Data:', imageField[0].src);
-        //     //                 // Set the item_name as the barcode value
-        //     //                 frm.doc.barcodes[0].custom_barcode_image = imageField[0].src;
-        //     //                 frm.refresh_field('barcodes');
-        //     //             } catch (error) {
-        //     //                 frappe.msgprint(__('Error generating barcode: {0}', [error.message]));
-        //     //             }
-        //     //         });
-        //     //     } else {
-        //     //         frappe.msgprint(__('Please enter the item name.'));
-        //     //     }
-        // });
-    }
-});
 
 frappe.ui.form.on("Item Barcode", {
-    barcode: function (frm, cdt, cdn) {
-        var doc = locals[cdt][cdn];
-        console.log("in barcode", doc.barcode);
-        generate_barcode(doc,frm, cdt, cdn)
-    },
-    barcode_type: function (frm, cdt, cdn) {
-        var doc = locals[cdt][cdn];
-        console.log("in barcode_type", doc.barcode_type);
-        generate_barcode(doc,frm, cdt, cdn)
-    },
-});
+    custom_create_barcode: function (frm, cdt, cdn) {
+        frappe.call({
+            method: "supplier_item_picker.api.create_code",
+            args: {
+                'number': frm.cur_grid.doc.barcode,
+                'item_code': frm.fields_dict.item_code.value,
+                'item_name': frm.fields_dict.item_name.value
 
-function generate_barcode(doc, frm, cdt, cdn) {
-    if (doc.barcode && doc.barcode_type == 'EAN' && frm.doc.item_name) {
-        console.log("in function");
-        $(frm.fields_dict['custom_childtable_linked_barcode'].wrapper).html('<svg id="ean13"></svg>');
-        $.getScript("https://cdn.jsdelivr.net/npm/jsbarcode@3.11.5/dist/JsBarcode.all.min.js", function (data, textStatus, jqxhr) {
-            JsBarcode("#ean13", frm.doc.item_name, {
-                background: "#FFFFFF"
-            });
-            var barcode_svg = $('#ean13').parent().html();
-            var full_content = `<div>
-                                ${barcode_svg.slice(0, -10)}
-                                <text style="font: 15px monospace" text-anchor="middle" x="85" y="111">${doc.barcode}</text>
-                                ${barcode_svg.slice(-10)}
-                                </div>`;
-            frappe.model.set_value(cdt, cdn, "custom_barcode_image", full_content);
-            frappe.model.set_value(cdt, cdn, "custom_barcode_svg", full_content);
+            },
+            callback: function (r) {
+                frappe.model.set_value(cdt, cdn, "barcode", r.message.message.barcode);
+
+                frappe.model.set_value(cdt, cdn, "custom_barcode_svg", r.message.message.image);
+                html = `<div><html><img src="data:image/png;base64,${r.message.message.image}"/></html></div>`;
+                console.log(html);
+                frappe.model.set_value(cdt, cdn, "custom_test_image", html);
+            }
         });
     }
-}
+});
